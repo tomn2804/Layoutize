36cdc81adc585366a3aa99fe223758d137768db6
minh2804 <minh2804@uw.edu>
2021-09-24 18:44:43 -0700

    Remove previous tests

---
+++ SchemataPreview.Tests/SchemaTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SchemataPreview.Tests
+{
+	public class DirectoryModelTests
+	{
+		[Fact]
+		public void Build_BaseCase_ReturnsModel()
+		{
+		}
+	}
+}

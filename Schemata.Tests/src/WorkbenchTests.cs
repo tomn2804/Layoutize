c9e92bcf751d0d8cd6437e9fb178ffb1230cc4c0
Tom Nguyen <tomn2804@gmail.com>
2021-11-05 15:08:49 -0700

    Init first test

---
+++ Schemata.Tests/src/WorkbenchTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using SchemataPreview;
+using System.Management.Automation;
+
+namespace Schemata.Tests
+{
+    public class WorkbenchTests
+    {
+        [Fact]
+        public void BuildMethod_EmptyFile_ReturnsMountedModel()
+        {
+            using PowerShell instance = PowerShell.Create();
+            Model model = (Model)instance.AddScript($@"
+            	using module Schemata
+            	using namespace Schemata
+
+            	class EmptyFileSchema : Schema<FileModel> {{
+                    EmptyFileSchema([ImmutableHashtable]$hashtable) : base($hashtable) {{}}
+
+                    [Schema]Build() {{
+                        return [FileSchema]$hashtable
+                    }}
+                }}
+
+            	$schema = [EmptyFileSchema]@{{ Name = 'Empty.txt' }}
+
+                [Workbench]::new($schema).Build('C:\Users\Tom\Desktop')
+            ").Invoke().Last().BaseObject;
+            Assert.True(model.Exists);
+        }
+    }
+}

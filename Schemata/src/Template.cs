e1f9b4069f3627afaf977d7468f4b20241c6bdb0
Tom Nguyen <tomn2804@gmail.com>
2021-12-09 16:41:32 -0800

    Remove implicit Blueprint cast

--- Schemata/src/Template.cs
+++ Schemata/src/Template.cs
@@ -21,10 +21,10 @@ public abstract partial class Model
 
     private Blueprint _blueprint;
 
-    protected Blueprint Blueprint
+    public Blueprint Blueprint
     {
         get => _blueprint;
-        private set
+        set
         {
             _blueprint = value;
             // Remount
@@ -40,17 +40,54 @@ public class FileModel : Model
     }
 }
 
+public abstract class Template
+{
+    protected Template(IEnumerable variables)
+    {
+        switch (variables)
+        {
+            case IImmutableDictionary<object, object?> dictionary:
+                Variables = dictionary;
+                break;
+            case IDictionary<object, object?> dictionary:
+                Variables = dictionary.ToImmutableDictionary();
+                break;
+            case IDictionary dictionary:
+                Variables = dictionary.Cast<DictionaryEntry>().ToImmutableDictionary(entry => entry.Key, entry => entry.Value);
+                break;
+            default:
+                int key = 0;
+                Variables = variables.Cast<object?>().ToImmutableDictionary(_ => (object)key++, value => value);
+                break;
+        }
+    }
+
+    public abstract Type ModelType { get; }
+
+    public IImmutableDictionary<object, object?> Variables { get; }
+
+    protected abstract Blueprint Inflate();
+
+    protected Blueprint InflateTo(Template template)
+    {
+        if (!ModelType.IsAssignableTo(template.ModelType))
+        {
+            throw new InvalidOperationException();
+        }
+        return template.Inflate();
+    }
+}
+
 public sealed partial class Blueprint
 {
     private Blueprint(BlankTemplate template)
     {
-        Template = template;
+        Variables = template.Variables;
+        ModelType = template.ModelType;
     }
 
-    private BlankTemplate Template { get; }
-
-    public IImmutableDictionary<object, object?> Variables => Template.Variables;
-    public Type ModelType => Template.ModelType;
+    public IImmutableDictionary<object, object?> Variables { get; }
+    public Type ModelType { get; }
 }
 
 public sealed partial class Blueprint
@@ -62,96 +99,15 @@ public sealed partial class Blueprint
         {
         }
 
-        protected override Blueprint Build()
+        protected override Blueprint Inflate()
         {
-            Debug.WriteLine(nameof(BlankTemplate));
+            Console.WriteLine(nameof(BlankTemplate));
             return new(this);
         }
     }
 }
 
-public class VariablesUpdatingEventArgs : EventArgs
-{
-    public VariablesUpdatingEventArgs(IImmutableDictionary<object, object?> variables)
-    {
-        Variables = variables;
-    }
-
-    public IImmutableDictionary<object, object?> Variables { get; }
-}
-
-public abstract partial class Model
-{
-    public abstract class Template
-    {
-        protected Template(IEnumerable variables)
-        {
-            switch (variables)
-            {
-                case IImmutableDictionary<object, object?> dictionary:
-                    _variables = dictionary;
-                    break;
-                case IDictionary<object, object?> dictionary:
-                    _variables = dictionary.ToImmutableDictionary();
-                    break;
-                case IDictionary dictionary:
-                    _variables = dictionary.Cast<DictionaryEntry>().ToImmutableDictionary(entry => entry.Key, entry => entry.Value);
-                    break;
-                default:
-                    int key = 0;
-                    _variables = variables.Cast<object?>().ToImmutableDictionary(_ => (object)key++, value => value);
-                    break;
-            }
-            Func<Model, bool> handleOnInitializing = (model) =>
-            {
-                if (!model.GetType().IsAssignableTo(ModelType))
-                {
-                    throw new InvalidOperationException();
-                }
-                void updateModel(object? sender, VariablesUpdatingEventArgs e)
-                {
-                    model.Blueprint = (Blueprint)Activator.CreateInstance(sender!.GetType(), e.Variables)!;
-                    VariablesUpdating -= updateModel;
-                }
-                VariablesUpdating += updateModel;
-                return ((Func<Model, bool>)Variables[TemplateVariables.OnInitializing]!)(model);
-            };
-            _variables = Variables.SetItem(TemplateVariables.OnInitializing, handleOnInitializing);
-        }
-
-        public abstract Type ModelType { get; }
-
-        private readonly IImmutableDictionary<object, object?> _variables;
-
-        public IImmutableDictionary<object, object?> Variables
-        {
-            get => _variables;
-            protected set
-            {
-                if (value != _variables)
-                {
-                    OnVariablesUpdating(new VariablesUpdatingEventArgs(value));
-                }
-            }
-        }
-
-        public event EventHandler<VariablesUpdatingEventArgs>? VariablesUpdating;
-
-        protected virtual void OnVariablesUpdating(VariablesUpdatingEventArgs e)
-        {
-            VariablesUpdating?.Invoke(this, e);
-        }
-
-        public static implicit operator Blueprint(Template template)
-        {
-            return template.Build();
-        }
-
-        protected abstract Blueprint Build();
-    }
-}
-
-public abstract class Template<T> : Model.Template where T : Model
+public abstract class Template<T> : Template where T : Model
 {
     protected Template(IEnumerable variables)
         : base(variables)
@@ -168,10 +124,10 @@ public class FileTemplate : Template<FileModel>
     {
     }
 
-    protected override Blueprint Build()
+    protected override Blueprint Inflate()
     {
-        Debug.WriteLine(nameof(FileTemplate));
-        return new Blueprint.BlankTemplate(Variables);
+        Console.WriteLine(nameof(FileTemplate));
+        return InflateTo(new Blueprint.BlankTemplate(Variables));
     }
 }
 
@@ -182,10 +138,10 @@ public class TextFileTemplate : Template<FileModel>
     {
     }
 
-    protected override Blueprint Build()
+    protected override Blueprint Inflate()
     {
-        Debug.WriteLine(nameof(TextFileTemplate));
-        return new FileTemplate(Variables);
+        Console.WriteLine(nameof(TextFileTemplate));
+        return InflateTo(new FileTemplate(Variables));
     }
 }
 
@@ -196,35 +152,35 @@ public class StrictTextFileTemplate : Template<FileModel>
     {
     }
 
-    protected override Blueprint Build()
+    protected override Blueprint Inflate()
     {
-        Debug.WriteLine(nameof(StrictTextFileTemplate));
-        return new TextFileTemplate(Variables);
+        Console.WriteLine(nameof(StrictTextFileTemplate));
+        return InflateTo(new TextFileTemplate(Variables));
     }
 }
 
-public class Workbench
-{
-    public Workbench(string path)
-    {
-        WorkingDirectoryPath = path;
-    }
+//public class Workbench
+//{
+//    public Workbench(string path)
+//    {
+//        WorkingDirectoryPath = path;
+//    }
 
-    public string WorkingDirectoryPath { get; }
+//    public string WorkingDirectoryPath { get; }
 
-    public Model Build(Model.Template template, params KeyValuePair<object, object?>[] variables)
-    {
-        if (variables.Any())
-        {
-            template = (Model.Template)Activator.CreateInstance(template.GetType(), template.Variables.SetItems(variables))!;
-        }
-        return Build(template);
-    }
+//    public Model Build(Template template, params KeyValuePair<object, object?>[] variables)
+//    {
+//        if (variables.Any())
+//        {
+//            template = (Template)Activator.CreateInstance(template.GetType(), template.Variables.SetItems(variables))!;
+//        }
+//        return Build(template);
+//    }
 
-    public Model Build(Blueprint blueprint)
-    {
-        Model model = (Model)Activator.CreateInstance(blueprint.ModelType, blueprint)!;
-        //model.Mount();
-        return model;
-    }
-}
+//    public Model Build(Blueprint blueprint)
+//    {
+//        Model model = (Model)Activator.CreateInstance(blueprint.ModelType, blueprint)!;
+//        //model.Mount();
+//        return model;
+//    }
+//}

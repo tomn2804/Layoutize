5bdc6a79d7e11af4bb5aed54f7d9929e799e46bf
Tom Nguyen <tomn2804@gmail.com>
2022-04-25 14:26:33 -0700

    Init tree

---
+++ Layoutize/src/Tree.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace Layoutize;
+
+public class Tree
+{
+    private Element Root { get; }
+
+    public Tree(Layout layout)
+    {
+        Root = layout.CreateElement();
+    }
+
+    public void Mount()
+    {
+        Root.MountTo(null);
+    }
+
+    public void MountTo(string path)
+    {
+        Directory.SetCurrentDirectory(path);
+        Root.MountTo(null);
+    }
+
+    public void Unmount()
+    {
+        Root.Unmount();
+    }
+}

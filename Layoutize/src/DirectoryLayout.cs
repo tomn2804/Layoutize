76ec9669ce8838ad297c869019d36b94ea5bf663
Tom Nguyen <tomn2804@gmail.com>
2022-05-21 21:14:03 -0700

    Fix Children attribute

--- Layoutize/src/DirectoryLayout.cs
+++ Layoutize/src/DirectoryLayout.cs
@@ -2,11 +2,26 @@
 using Layoutize.Contexts;
 using Layoutize.Views;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Layoutize;
 
 public class DirectoryLayout : ViewGroupLayout
 {
+    public new object Children
+    {
+        get => base.Children;
+        init
+        {
+            base.Children = value switch
+            {
+                IEnumerable<object> children => children.Cast<Layout>(),
+                _ => new[] { (Layout)value },
+            };
+        }
+    }
+
     internal override DirectoryElement CreateElement()
     {
         return new(this);

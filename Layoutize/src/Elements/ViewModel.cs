1f56e4d33f6344c0daf45296070e634ad6797e65
Tom Nguyen <tomn2804@gmail.com>
2022-05-30 00:11:49 -0700

    Add ViewModel

---
+++ Layoutize/src/Elements/ViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using Layoutize.Layouts;
+
+namespace Layoutize.Elements;
+
+internal sealed class ViewModel
+{
+	public Layout Layout
+	{
+		get => _layout;
+		set
+		{
+			value.Validate();
+			OnValueUpdating(EventArgs.Empty);
+			_layout = value;
+			OnValueUpdated(EventArgs.Empty);
+		}
+	}
+
+	public ViewModel(Layout layout)
+	{
+		Debug.Assert(layout.IsValid());
+		_layout = layout;
+	}
+
+	public event EventHandler? LayoutUpdated;
+
+	public event EventHandler? LayoutUpdating;
+
+	private void OnValueUpdated(EventArgs e)
+	{
+		LayoutUpdated?.Invoke(this, e);
+	}
+
+	private void OnValueUpdating(EventArgs e)
+	{
+		LayoutUpdating?.Invoke(this, e);
+	}
+
+	private Layout _layout;
+}
